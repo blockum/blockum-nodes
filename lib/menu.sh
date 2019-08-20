008a6980c0f51935e475d00a7885b17fc6110b7b
Freddy <freddy.figueroa.silva@gmail.com>
2019-08-20 12:53:34 -0500

    delete QM change of README

--- lib/menu.sh
+++ lib/menu.sh
@@ -4,20 +4,9 @@ source qm.variables
 source lib/common.sh
 
 function banner() {
-	printf $CYAN'   ______'$RED'   __      __\n' 
-	printf $CYAN'  / ____ \'$RED' |  \    /  |  \n'
-	printf $CYAN' / /    \ \'$RED'|   \  /   |  \n'
-	printf $CYAN' | |     | '$RED'|    \/    |  \n'
-	printf $CYAN' | |     | '$RED'| |\    /| |  \n'
-	printf $CYAN' | |     |  \'$RED'| \  / | |  \n'
-	printf $CYAN' \ \____/ /\ \'$RED'  \/  | |  \n'
-	printf $CYAN'  \______/  \_\'$RED'     |_|  '
-
-	local __version=$(egrep -Eo "[0-9].*" <<< $dockerImage)
-	    
+	local __version=$(egrep -Eo "[0-9].*" <<< $dockerImage)	    
 	IFS='_'; arrIN=($__version); unset IFS;
-
-	echo -e $GREEN'Version '${arrIN[1]}' Built on Quorum '${arrIN[0]}'\n'
+	echo -e $GREEN'BLOCKUM Iniciativa - Version 1.1.1  - Testnet v 1.1.1\n'
 }
 
 function readParameters() {
